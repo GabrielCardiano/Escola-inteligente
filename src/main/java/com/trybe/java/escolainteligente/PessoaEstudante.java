bdb088816ccd358eda5314f77e8634685f4c0b1a
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-01-19 04:50:40 -0300

    feat: req 1 - cria métodos calcularIdadeEmDias e coletarInformações da classe PessoaEstudante

--- src/main/java/com/trybe/java/escolainteligente/PessoaEstudante.java
+++ src/main/java/com/trybe/java/escolainteligente/PessoaEstudante.java
@@ -8,14 +8,30 @@ public class PessoaEstudante {
    * Método calcularIdadeEmDias.
    */
   public static int calcularIdadeEmDias(int anos, int meses, int dias) {
-    System.out.println("TODO: Implementar a função calcular Idade em Dias.");
-    return 0;
+    int idadeEmDias = (anos * 365) + (meses * 60) + dias;
+    System.out.println("Sua idade e, dias é: " + idadeEmDias);
+    return idadeEmDias;
   }
 
   /**
    * Método coletarInformacoes.
    */
   public static void coletarInformacoes() {
-    System.out.println("TODO: Solicitar informações da Pessoa Estudante ");
+    System.out.println("Qual o nome da Pessoa Estudante?");
+    Scanner scanData = new Scanner(System.in);
+    String nome = scanData.next();
+
+    System.out.println("Qual a sua idade em anos, meses e dias?");
+    System.out.println("anos:");
+    int anos = scanData.nextInt();
+
+    System.out.println("meses:");
+    int meses = scanData.nextInt();
+
+    System.out.println("dias:");
+    int dias = scanData.nextInt();
+
+    int idade = calcularIdadeEmDias(anos, meses, dias);
+    System.out.println("A idade de " + nome + " em dias é " + idade);
   }
 }
