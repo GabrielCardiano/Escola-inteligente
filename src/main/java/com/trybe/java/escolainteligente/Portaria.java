1da02ea542867d97e0fa2cb08fd79be4446818ca
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-01-19 07:32:56 -0300

    feat: req 3 - cria métodos coletarINformacoes e emitirRelatorio na classe Portaria

--- src/main/java/com/trybe/java/escolainteligente/Portaria.java
+++ src/main/java/com/trybe/java/escolainteligente/Portaria.java
@@ -1,6 +1,6 @@
 package com.trybe.java.escolainteligente;
 
-import java.text.DecimalFormat;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Portaria {
@@ -9,13 +9,77 @@ public class Portaria {
    * Método emitirRelatorio.
    */
   public static void emitirRelatorio(int qtdFundamental1, int qtdFundamental2, int qtdMedio) {
-    System.out.println("TODO: Implementar a função para gerar o relatório de acesso.");
+    int totalAlunos = qtdFundamental1 + qtdFundamental2 + qtdMedio;
+    Locale.setDefault(Locale.US);
+
+    //    Número percentual de alunos
+    double porcFundamental1 = 0d;
+    double porcFundamental2 = 0d;
+    double porcMedio = 0d;
+
+    if (totalAlunos > 0) {
+      porcFundamental1 = ((double) qtdFundamental1 / totalAlunos) * 100;
+      porcFundamental2 = ((double) qtdFundamental2 / totalAlunos) * 100;
+      porcMedio = ((double) qtdMedio / totalAlunos) * 100;
+    }
+
+    System.out.println("----- Quantidade -----");
+    System.out.println("Ensino Fundamental I: " + qtdFundamental1);
+    System.out.println("Ensino Fundamental II: " + qtdFundamental2);
+    System.out.println("Ensino Médio: " + qtdMedio + "\n");
+
+    System.out.println("----- Percentual -----");
+    System.out.println("Ensino Fundamental I: " + String.format("%.1f", porcFundamental1) + "%");
+    System.out.println("Ensino Fundamental II: " + String.format("%.1f", porcFundamental2) + "%");
+    System.out.println("Ensino Médio: " + String.format("%.1f", porcMedio) + "%" + "\n");
+
+    System.out.println("TOTAL: " + totalAlunos);
   }
 
   /**
    * Método coletarInformacoes.
    */
   public static void coletarInformacoes() {
-    System.out.println("TODO: Solicitar informações para controle de acesso ");
+    Scanner scanner = new Scanner(System.in);
+
+    int qtdFundamental1 = 0;
+    int qtdFundamental2 = 0;
+    int qtdMedio = 0;
+    boolean loop = true;
+
+    //    Autoriza estudante e coleta info de faixa etária;
+    while (loop) {
+      System.out.println("Entre com o número correspondente à opção desejada:");
+      System.out.println("1 - Registrar o acesso de pessoa estudante");
+      System.out.println("2 - Finalizar o acesso e emitir o relatório");
+
+      int option = scanner.nextInt();
+
+      if (option == 1) {
+        System.out.println("Entre com a idade da pessoa estudante:");
+        int idade = scanner.nextInt();
+
+        if (idade <= 10) {
+          System.out.println("Pessoa estudante do Ensino Fundamental I, catraca liberada!");
+          qtdFundamental1++;
+        } else if (idade >= 11 && idade <= 14) {
+          System.out.println("Pessoa estudante do Ensino Fundamental II, catraca liberada!");
+          qtdFundamental2++;
+        } else {
+          System.out.println("Pessoa estudante do Ensino Médio, catraca liberada!");
+          qtdMedio++;
+        }
+      } else if (option == 2) {
+        loop = false;
+      } else {
+        System.out.println("Entre com uma opção válida!");
+      }
+    }
+
+    // Todo: emitir relatório
+    emitirRelatorio(qtdFundamental1, qtdFundamental2, qtdMedio);
+
+    scanner.close();
   }
 }
+
